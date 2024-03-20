b122bde62c9cdd928c00eb7e14cfcb35db538faa
Cody Oram <cody.oram@keyin.com>
2024-03-20 15:50:46 -0230

    march20th

--- Week12/scripts/fetch.js
+++ Week12/scripts/fetch.js
@@ -1,1 +1,33 @@
 // FETCH
+
+// console.log(fetch("./data/data.txt"));
+
+// fetch("./data/data.txt")
+//   .then((res) => {
+//     return res.text();
+//   })
+//   .then((data) => {
+//     console.log(data);
+//   });
+
+document.querySelector("#btn1").addEventListener("click", loadData);
+
+function loadData() {
+  fetch("./data/data1.txt")
+    .then((res, rej) => {
+      if (res.status !== 200) {
+        return rej("Error");
+      } else {
+        return res.text();
+      }
+    })
+    .catch((message) => {
+      document.querySelector("#output").innerHTML = `ERROR: ${message}`;
+    })
+    .then((data) => {
+      //   console.log(data);
+      document.querySelector(
+        "#output"
+      ).innerHTML = `The data received is: ${data}`;
+    });
+}
--- Week12/scripts/promises.js
+++ Week12/scripts/promises.js
@@ -1,18 +1,24 @@
 // PROMISES
 // how to define a promise
-let p = new Promise((resolve, reject) => {
-  let a = 2 + 2;
 
-  if (a == 4) {
-    resolve("success");
-  } else {
-    reject("failed");
-  }
-});
+// let p = new Promise((resolve, reject) => {
+//   let a = 2 + 2;
+
+//   if (a == 4) {
+//     resolve("success");
+//   } else {
+//     reject("failed");
+//   }
+// });
 
 // how to use/call a promise
+// p.then().catch(); if resolve() ..then executes... if reject() ...catch executes
 
-p.then((message) => {});
+// p.then((message) => {
+//   console.log("This is the then " + message);
+// }).catch((message) => {
+//   console.log("This is the catch" + message);
+// });
 
 let posts = [
   {
@@ -32,17 +38,38 @@ let posts = [
   },
 ];
 
-function createPost(post) {}
+function createPost(post) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      posts.push(post);
+
+      let error = true;
+      if (!error) {
+        resolve();
+      } else {
+        reject("Error:Something bad happened");
+      }
+    }, 3000);
+  });
+}
 
 function getPosts() {
   setTimeout(() => {
     posts.forEach((post) => {
       document.querySelector(
-        "output"
+        "#output"
       ).innerHTML += `<ul><li>${post.id}</li><li>${post.author}</li><li>${post.text}</li></ul>`;
     });
   }, 1000);
 }
 
-createPost({ id: 4, author: "author4", text: "Post4" }); //taking 3 seconds
-getPosts(); // takes 1 second to execute..
+// createPost({ id: 4, author: "author4", text: "Post4" });
+// getPosts();
+
+createPost({ id: 4, author: "author4", text: "Post4" })
+  .then(getPosts)
+  .catch((message) => {
+    document.querySelector(
+      "#output"
+    ).innerHTML = `ERROR: LOOK AT THE MESSAGE: ${message}`;
+  });
